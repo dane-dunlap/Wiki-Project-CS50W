827b6c13ce1153b51072c7c79103a863be8c24ff
dane-dunlap <danecooperdunlap@gmail.com>
2023-01-11 22:26:35 +0400

    added search and create functionality

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("entry",views.entry,name="entry")
+    path("<str:title>",views.entry,name="entry"),
+    path("search/",views.search,name="search"),
+    path("create/",views.create,name="create")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,13 +1,56 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from markdown2 import Markdown
 
 from . import util
 
+markdowner = Markdown()
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
-def entry(request):
-    return HttpResponse("Hello entry page")
+def entry(request,title):
+    entry = util.get_entry(title)
+    if entry == None:
+        return render(request,"encyclopedia/error.html")
+    
+    return render(request,"encyclopedia/entry.html", {
+        "content":markdowner.convert(entry),"title":title
+    })
+
+def search(request):
+    if request.method == "GET":
+        title = request.GET['q']
+        entry = util.get_entry(title)
+        if entry == None:
+            all_entries = util.list_entries()
+            search_results = []
+            for entry in all_entries:
+                if title in entry:
+                    search_results.append(entry)
+            if search_results:
+                return render(request,"encyclopedia/search_results.html",{
+                "search_results":search_results
+            })
+            else:
+                return render(request,"encyclopedia/no_search.html")
+        
+        return render(request,"encyclopedia/entry.html", {
+        "content":markdowner.convert(entry),"title":title
+    })
+
+def create(request):
+    if request.method =="GET":
+        return render(request,"encyclopedia/create.html")
+
+    if request.method == "POST":
+        title = request.POST['title']
+        content = request.POST['content']
+        util.save_entry(title,content)
+        new_entry = util.get_entry(title)
+        
+        return render(request,"encyclopedia/entry.html", {
+        "content":markdowner.convert(new_entry),"title":title
+    })
